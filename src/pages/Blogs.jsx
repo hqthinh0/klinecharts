c1467dd41aa47c7ead8432536608487c7150084d
hqthinh0@gmail.com <hqthinh0@gmail.com>
2025-06-25 09:53:34 +0700

    text-update

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -7,7 +7,7 @@ const Blogs = () => {
 
     <ContextProviderOne>
         <NewPage />
-        update branch checkout master
+        update branch checkout masterss
     </ContextProviderOne>
    
   )
